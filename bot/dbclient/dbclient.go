197c63dd26cf378eda396c31460928e83677582b
Dot-Rar <dotrar@protonmail.com>
2020-05-30 17:41:29 +0100

    build config

--- bot/dbclient/dbclient.go
+++ bot/dbclient/dbclient.go
@@ -3,18 +3,34 @@ package dbclient
 import (
 	"context"
 	"github.com/TicketsBot/database"
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/log/logrusadapter"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/sirupsen/logrus"
 	"os"
+	"strconv"
 )
 
 var Client *database.Database
 
 func Connect() {
-	config, err := pgxpool.ParseConfig(os.Getenv("WORKER_PG_URI")); if err != nil {
+	maxConns, err := strconv.Atoi(os.Getenv("WORKER_PG_THREADS"))
+	if err != nil {
 		panic(err)
+	}
+
+	config := &pgxpool.Config{
+		ConnConfig: &pgx.ConnConfig{
+			Config: pgconn.Config{
+				Host:     os.Getenv("WORKER_PG_HOST"),
+				Port:     5432,
+				Database: os.Getenv("WORKER_PG_DATABASE"),
+				User:     os.Getenv("WORKER_PG_USER"),
+				Password: os.Getenv("WORKER_PG_PASSWD"),
+			},
+		},
+		MaxConns: int32(maxConns),
 	}
 
 	// TODO: Sentry
@@ -29,4 +45,3 @@ func Connect() {
 	Client = database.NewDatabase(pool)
 	//Client.CreateTables(pool)
 }
-
