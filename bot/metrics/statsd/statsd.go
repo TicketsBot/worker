23c44426c9da195096aa0d90147f5463691acbe6
Dot-Rar <dotrar@protonmail.com>
2020-06-03 19:51:17 +0100

    fix statsd address

--- bot/metrics/statsd/statsd.go
+++ bot/metrics/statsd/statsd.go
@@ -1,7 +1,6 @@
 package statsd
 
 import (
-	"fmt"
 	stats "gopkg.in/alexcesaro/statsd.v2"
 	"os"
 )
@@ -13,8 +12,7 @@ type StatsdClient struct {
 var Client StatsdClient
 
 func NewClient() (StatsdClient, error) {
-	addr := fmt.Sprintf("%s:%d", os.Getenv("WORKER_STATSD_HOST"), os.Getenv("WORKER_STATSD_PORT"))
-	client, err := stats.New(stats.Address(addr), stats.Prefix(os.Getenv("WORKER_STATSD_PREFIX"))); if err != nil {
+	client, err := stats.New(stats.Address(os.Getenv("WORKER_STATSD_ADDR")), stats.Prefix(os.Getenv("WORKER_STATSD_PREFIX"))); if err != nil {
 		return StatsdClient{}, err
 	}
 
--- go.mod
+++ go.mod
@@ -7,8 +7,6 @@ require (
 	github.com/TicketsBot/common v0.0.0-20200602202048-9d7645170a49
 	github.com/TicketsBot/database v0.0.0-20200602215845-296c7c10921c
 	github.com/elliotchance/orderedmap v1.2.1
-	github.com/getsentry/raven-go v0.2.0
-	github.com/go-errors/errors v1.1.0
 	github.com/go-redis/redis v6.15.8+incompatible
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/jackc/pgtype v1.3.0
