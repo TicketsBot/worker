ab9b60991bb507347407760f33b5fe6927e20aad
Dot-Rar <dotrar@protonmail.com>
2020-06-09 18:28:46 +0100

    latest

--- bot/listeners/memberleave.go
+++ bot/listeners/memberleave.go
@@ -24,13 +24,14 @@ func OnMemberLeave(worker *worker.Context, e *events.GuildMemberRemove, extra ev
 	if err != nil {
 		sentry.Error(err)
 	} else {
+		// check setting is neabled
 		if settings.Enabled && settings.OnUserLeave != nil && *settings.OnUserLeave {
+			// get open tickets by user
 			tickets, err := dbclient.Client.Tickets.GetOpenByUser(e.GuildId, e.User.Id)
 			if err != nil {
 				sentry.Error(err)
 			} else {
 				for _, ticket := range tickets {
-
 					// verify ticket exists + prevent potential panic
 					if ticket.ChannelId == nil {
 						return
