9b3cdd95b8bf1b86cb545db8fe78c4f273695732
rxdn <dotrar@protonmail.com>
2020-06-17 22:25:29 +0100

    do db lookups in parallel

--- bot/utils/welcomemessage.go
+++ bot/utils/welcomemessage.go
@@ -1,13 +1,18 @@
 package utils
 
 import (
+	"context"
 	"fmt"
 	"github.com/TicketsBot/common/sentry"
 	"github.com/TicketsBot/database"
 	"github.com/TicketsBot/worker"
 	"github.com/TicketsBot/worker/bot/dbclient"
+	"github.com/rxdn/gdl/objects/guild"
+	"github.com/rxdn/gdl/objects/user"
+	"golang.org/x/sync/errgroup"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -53,58 +58,105 @@ func SendWelcomeMessage(worker *worker.Context, guildId, channelId, userId uint6
 }
 
 func doSubstitutions(welcomeMessage string, worker *worker.Context, guildId, userId, channelId uint64, ticketId int) string {
+	var lock sync.Mutex
+
+	// do substitutions that do not require DB lookups first
+
 	// %user%
 	welcomeMessage = strings.Replace(welcomeMessage, "%user%", fmt.Sprintf("<@%d>", userId), -1)
-
-	// %username%
-	if strings.Contains(welcomeMessage, "%username%") {
-		if user, err := worker.GetUser(userId); err == nil {
-			welcomeMessage = strings.Replace(welcomeMessage, "%username%", user.Username, -1)
-		}
-	}
-
-	// %server%
-	if strings.Contains(welcomeMessage, "%server%") {
-		if guild, err := worker.GetGuild(guildId); err == nil {
-			welcomeMessage = strings.Replace(welcomeMessage, "%server%", guild.Name, -1)
-		}
-	}
 
 	// %ticket_id%
 	if ticketId > 0 {
 		welcomeMessage = strings.Replace(welcomeMessage, "%ticket_id%", strconv.Itoa(ticketId), -1)
 	}
 
+	// %channel%
+	welcomeMessage = strings.Replace(welcomeMessage, "%channel%", fmt.Sprintf("<#%d>", channelId), -1)
+
+	// do DB lookups in parallel
+	group, _ := errgroup.WithContext(context.Background())
+
+	// %username%
+	if strings.Contains(welcomeMessage, "%username%") {
+		group.Go(func() (err error) {
+			var user user.User
+			if user, err = worker.GetUser(userId); err == nil {
+				lock.Lock()
+				welcomeMessage = strings.Replace(welcomeMessage, "%username%", user.Username, -1)
+				lock.Unlock()
+			}
+			return
+		})
+	}
+
+	// %server%
+	if strings.Contains(welcomeMessage, "%server%") {
+		group.Go(func() (err error) {
+			var guild guild.Guild
+			if guild, err = worker.GetGuild(guildId); err == nil {
+				lock.Lock()
+				welcomeMessage = strings.Replace(welcomeMessage, "%server%", guild.Name, -1)
+				lock.Unlock()
+			}
+			return
+		})
+	}
+
 	// %open_tickets%
 	if strings.Contains(welcomeMessage, "%open_tickets%") {
-		if open, err := dbclient.Client.Tickets.GetGuildOpenTickets(guildId); err == nil {
-			welcomeMessage = strings.Replace(welcomeMessage, "%open_tickets%", strconv.Itoa(len(open)), -1)
-		}
+		group.Go(func() (err error) {
+			var open []database.Ticket
+			if open, err = dbclient.Client.Tickets.GetGuildOpenTickets(guildId); err == nil {
+				lock.Lock()
+				welcomeMessage = strings.Replace(welcomeMessage, "%open_tickets%", strconv.Itoa(len(open)), -1)
+				lock.Unlock()
+			}
+			return
+		})
 	}
 
 	// %total_tickets%
 	if strings.Contains(welcomeMessage, "%total_tickets%") {
-		if count, err := dbclient.Client.Tickets.GetTotalTicketCount(guildId); err == nil {
-			welcomeMessage = strings.Replace(welcomeMessage, "%total_tickets%", strconv.Itoa(count), -1)
-		}
+		group.Go(func() (err error) {
+			var count int
+			if count, err = dbclient.Client.Tickets.GetTotalTicketCount(guildId); err == nil {
+				lock.Lock()
+				welcomeMessage = strings.Replace(welcomeMessage, "%total_tickets%", strconv.Itoa(count), -1)
+				lock.Unlock()
+			}
+			return
+		})
 	}
 
 	// %user_open_tickets%
 	if strings.Contains(welcomeMessage, "%user_open_tickets%") {
-		if open, err := dbclient.Client.Tickets.GetOpenByUser(guildId, userId); err == nil {
-			welcomeMessage = strings.Replace(welcomeMessage, "%user_open_tickets%", strconv.Itoa(len(open)), -1)
-		}
+		group.Go(func() (err error) {
+			var open []database.Ticket
+			if open, err = dbclient.Client.Tickets.GetOpenByUser(guildId, userId); err == nil {
+				lock.Lock()
+				welcomeMessage = strings.Replace(welcomeMessage, "%user_open_tickets%", strconv.Itoa(len(open)), -1)
+				lock.Unlock()
+			}
+			return
+		})
 	}
 
 	// %ticket_limit%
 	if strings.Contains(welcomeMessage, "%ticket_limit%") {
-		if limit, err := dbclient.Client.TicketLimit.Get(guildId); err == nil {
-			welcomeMessage = strings.Replace(welcomeMessage, "%ticket_limit%", strconv.Itoa(int(limit)), -1)
-		}
+		group.Go(func() (err error) {
+			var limit uint8
+			if limit, err = dbclient.Client.TicketLimit.Get(guildId); err == nil {
+				lock.Lock()
+				welcomeMessage = strings.Replace(welcomeMessage, "%ticket_limit%", strconv.Itoa(int(limit)), -1)
+				lock.Unlock()
+			}
+			return
+		})
 	}
 
-	// %channel%
-	welcomeMessage = strings.Replace(welcomeMessage, "%channel%", fmt.Sprintf("<#%d>", channelId), -1)
+	if err := group.Wait(); err != nil {
+		sentry.Error(err)
+	}
 
 	return welcomeMessage
 }
