10e6075138a8153ace862bd2821e5ca078d6f159
rxdn <dotrar@protonmail.com>
2020-06-22 23:11:29 +0100

    delete

--- bot/listeners/delete.go
+++ bot/listeners/delete.go
@@ -9,7 +9,18 @@ import (
 )
 
 func OnChannelDelete(worker *worker.Context, e *events.ChannelDelete, extra eventforwarding.Extra) {
+	// if this is an ticket channel, close it
 	if err := dbclient.Client.Tickets.CloseByChannel(e.Id); err != nil {
+		sentry.Error(err)
+	}
+
+	// if this is a channel category, delete it
+	if err := dbclient.Client.ChannelCategory.DeleteByChannel(e.Id); err != nil {
+		sentry.Error(err)
+	}
+
+	// if this is an archive channel, delete it
+	if err := dbclient.Client.ArchiveChannel.DeleteByChannel(e.Id); err != nil {
 		sentry.Error(err)
 	}
 }
