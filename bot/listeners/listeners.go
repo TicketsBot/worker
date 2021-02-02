c6fe912e4c81c762308a3484d46bbe30caba146c
rxdn <29165304+rxdn@users.noreply.github.com>
2021-02-02 20:56:45 +0000

    Add reaction back

--- bot/listeners/listeners.go
+++ bot/listeners/listeners.go
@@ -3,10 +3,11 @@ package listeners
 import "github.com/rxdn/gdl/gateway/payloads/events"
 
 var Listeners = map[events.EventType][]interface{}{
-	events.CHANNEL_DELETE:       {OnChannelDelete},
-	events.MESSAGE_REACTION_ADD: {OnCloseConfirm, OnCloseReact, OnMultiPanelReact, OnPanelReact, OnViewStaffReact},
-	events.MESSAGE_CREATE:       {OnCommand, OnMessage, OnSetupProgress},
-	events.GUILD_CREATE:         {OnGuildCreate},
-	events.GUILD_DELETE:         {OnGuildLeave},
-	events.GUILD_MEMBER_REMOVE:  {OnMemberLeave},
+	events.CHANNEL_DELETE:             {OnChannelDelete},
+	events.MESSAGE_REACTION_ADD:       {OnCloseConfirm, OnCloseReact, OnMultiPanelReact, OnPanelReact, OnViewStaffReact},
+	events.MESSAGE_REATION_REMOVE_ALL: {OnReactionRemove},
+	events.MESSAGE_CREATE:             {OnCommand, OnMessage, OnSetupProgress},
+	events.GUILD_CREATE:               {OnGuildCreate},
+	events.GUILD_DELETE:               {OnGuildLeave},
+	events.GUILD_MEMBER_REMOVE:        {OnMemberLeave},
 }
