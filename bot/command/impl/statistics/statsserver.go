ed4dec21fcee1319bb03834d90352557f152fa4b
rxdn <29165304+rxdn@users.noreply.github.com>
2021-02-16 11:08:27 +0000

    Remove debug

--- bot/command/impl/statistics/statsserver.go
+++ bot/command/impl/statistics/statsserver.go
@@ -3,7 +3,6 @@ package statistics
 import (
 	"context"
 	"github.com/TicketsBot/common/permission"
-	"github.com/TicketsBot/common/sentry"
 	translations "github.com/TicketsBot/database/translations"
 	"github.com/TicketsBot/worker/bot/command"
 	"github.com/TicketsBot/worker/bot/dbclient"
@@ -34,18 +33,11 @@ func (c StatsServerCommand) GetExecutor() interface{} {
 func (StatsServerCommand) Execute(ctx command.CommandContext) {
 	var totalTickets, openTickets int
 
-	tags := map[string]string{
-		"guild": strconv.FormatUint(ctx.GuildId(), 10),
-	}
-
-	sentry.LogWithTags("stats 1", nil, tags)
-
 	group, _ := errgroup.WithContext(context.Background())
 
 	// totalTickets
 	group.Go(func() (err error) {
 		totalTickets, err = dbclient.Client.Tickets.GetTotalTicketCount(ctx.GuildId())
-		sentry.LogWithTags("stats 2", map[string]interface{}{"total_tickets": totalTickets}, tags)
 		return
 	})
 
@@ -53,7 +45,6 @@ func (StatsServerCommand) Execute(ctx command.CommandContext) {
 	group.Go(func() error {
 		tickets, err := dbclient.Client.Tickets.GetGuildOpenTickets(ctx.GuildId())
 		openTickets = len(tickets)
-		sentry.LogWithTags("stats 3", map[string]interface{}{"open_tickets": openTickets}, tags)
 		return err
 	})
 
@@ -63,21 +54,18 @@ func (StatsServerCommand) Execute(ctx command.CommandContext) {
 	// total
 	group.Go(func() (err error) {
 		total, err = dbclient.Client.FirstResponseTime.GetAverageAllTime(ctx.GuildId())
-		sentry.LogWithTags("stats 4", map[string]interface{}{"first_response_total": total}, tags)
 		return
 	})
 
 	// monthly
 	group.Go(func() (err error) {
 		monthly, err = dbclient.Client.FirstResponseTime.GetAverage(ctx.GuildId(), time.Hour * 24 * 28)
-		sentry.LogWithTags("stats 5", map[string]interface{}{"first_response_monthly": monthly}, tags)
 		return
 	})
 
 	// weekly
 	group.Go(func() (err error) {
 		weekly, err = dbclient.Client.FirstResponseTime.GetAverage(ctx.GuildId(), time.Hour * 24 * 7)
-		sentry.LogWithTags("stats 6", map[string]interface{}{"first_response_weekly": weekly}, tags)
 		return
 	})
 
@@ -119,7 +107,6 @@ func (StatsServerCommand) Execute(ctx command.CommandContext) {
 		AddField("Average First Response Time (Monthly)", monthlyFormatted, true).
 		AddField("Average First Response Time (Weekly)", weeklyFormatted, true)
 
-	sentry.LogWithTags("stats 7", nil, tags)
 	ctx.ReplyWithEmbed(embed)
 	ctx.Accept()
 }
--- bot/utils/timeutils.go
+++ bot/utils/timeutils.go
@@ -9,7 +9,7 @@ func FormatTime(interval time.Duration) string {
 	minutes := (interval.Milliseconds() / (1000 * 60)) % 60
 	hours := (interval.Milliseconds() / (1000 * 60 * 60)) % 24
 
-	return fmt.Sprintf("%dh %02d", hours, minutes)
+	return fmt.Sprintf("%dh %02dm", hours, minutes)
 }
 
 func FormatDateTime(time time.Time) string {
