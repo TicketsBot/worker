f938673453ca669ab4fc462c966d1cb8beb72f0f
rxdn <29165304+rxdn@users.noreply.github.com>
2021-06-07 14:56:24 +0100

    Remove reaction listeners

--- bot/listeners/listeners.go
+++ bot/listeners/listeners.go
@@ -4,8 +4,7 @@ import "github.com/rxdn/gdl/gateway/payloads/events"
 
 var Listeners = map[events.EventType][]interface{}{
 	events.CHANNEL_DELETE:             {OnChannelDelete},
-	events.MESSAGE_REACTION_ADD:       {OnMultiPanelReact, OnPanelReact, OnViewStaffReact},
-	events.MESSAGE_REATION_REMOVE_ALL: {OnReactionRemove},
+	events.MESSAGE_REACTION_ADD:       {OnViewStaffReact},
 	events.MESSAGE_CREATE:             {GetCommandListener(), OnMessage, OnSetupProgress},
 	events.GUILD_CREATE:               {OnGuildCreate},
 	events.GUILD_DELETE:               {OnGuildLeave},
