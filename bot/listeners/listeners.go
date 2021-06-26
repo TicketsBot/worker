ccd081b371a87bbe5498e754d830a15a70575386
rxdn <29165304+rxdn@users.noreply.github.com>
2021-06-26 23:07:50 +0100

    Role delete listener

--- bot/listeners/listeners.go
+++ bot/listeners/listeners.go
@@ -3,10 +3,11 @@ package listeners
 import "github.com/rxdn/gdl/gateway/payloads/events"
 
 var Listeners = map[events.EventType][]interface{}{
-	events.CHANNEL_DELETE:             {OnChannelDelete},
-	events.MESSAGE_REACTION_ADD:       {OnViewStaffReact},
-	events.MESSAGE_CREATE:             {GetCommandListener(), OnMessage, OnSetupProgress},
-	events.GUILD_CREATE:               {OnGuildCreate},
-	events.GUILD_DELETE:               {OnGuildLeave},
-	events.GUILD_MEMBER_REMOVE:        {OnMemberLeave},
+	events.CHANNEL_DELETE:       {OnChannelDelete},
+	events.MESSAGE_REACTION_ADD: {OnViewStaffReact},
+	events.MESSAGE_CREATE:       {GetCommandListener(), OnMessage, OnSetupProgress},
+	events.GUILD_CREATE:         {OnGuildCreate},
+	events.GUILD_DELETE:         {OnGuildLeave},
+	events.GUILD_MEMBER_REMOVE:  {OnMemberLeave},
+	events.GUILD_ROLE_DELETE:    {OnRoleDelete},
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/TicketsBot/archiverclient v0.0.0-20210220155137-a562b2f1bbbb
 	github.com/TicketsBot/common v0.0.0-20210604175952-03cfa14c16e1
-	github.com/TicketsBot/database v0.0.0-20210625163257-35eea2daa97c
+	github.com/TicketsBot/database v0.0.0-20210626220541-fad712cc1c70
 	github.com/elliotchance/orderedmap v1.2.1
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-redis/redis v6.15.8+incompatible
